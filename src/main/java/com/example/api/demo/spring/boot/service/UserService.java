ab147f18f84418d0a45b9f1d49c896a8684c705c
Duong Dung <dungdcth@gmail.com>
2024-03-11 23:12:01 +0700

    CRUD user

---
+++ src/main/java/com/example/api/demo/spring/boot/service/UserService.java
@@ -0,0 +1,53 @@
+package com.example.api.demo.spring.boot.service;
+
+import com.example.api.demo.spring.boot.dto.UserCreateRequest;
+import com.example.api.demo.spring.boot.dto.UserUpdateRequest;
+import com.example.api.demo.spring.boot.entity.User;
+import com.example.api.demo.spring.boot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreateRequest request) {
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        User user = getUser(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
---
+++ src/test/java/com/example/api/demo/spring/boot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.demo.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
