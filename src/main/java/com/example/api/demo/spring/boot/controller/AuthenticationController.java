e4faccc2848149ae13b3c373bd74f10adcce9988
Duong Dung <dungdcth@gmail.com>
2024-03-21 23:43:33 +0700

    Auth login

--- src/main/java/com/example/api/demo/spring/boot/controller/AuthenticationController.java
+++ src/main/java/com/example/api/demo/spring/boot/controller/AuthenticationController.java
@@ -2,24 +2,28 @@ package com.example.api.demo.spring.boot.controller;
 
 import com.example.api.demo.spring.boot.dto.ApiResponse;
 import com.example.api.demo.spring.boot.dto.UserLoginRequest;
-import com.example.api.demo.spring.boot.entity.User;
+import com.example.api.demo.spring.boot.dto.response.AuthenticationResponse;
 import com.example.api.demo.spring.boot.service.AuthenticationService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/auth")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationController {
-    @Autowired
-    private AuthenticationService authenticationService;
+    AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 
     @PostMapping
-    ApiResponse<User> loginUser(@RequestBody @Valid UserLoginRequest request) {
-        ApiResponse<User> apiResponse = new ApiResponse<>();
-        apiResponse.setResult(authenticationService.loginUser(request));
-
-        return apiResponse;
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody @Valid UserLoginRequest request) {
+        boolean rs = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder().result(AuthenticationResponse.builder().authenticated(rs).build()).build();
     }
 
 }
--- src/main/java/com/example/api/demo/spring/boot/service/AuthenticationService.java
+++ src/main/java/com/example/api/demo/spring/boot/service/AuthenticationService.java
@@ -6,33 +6,29 @@ import com.example.api.demo.spring.boot.exception.AppException;
 import com.example.api.demo.spring.boot.exception.ErrorCode;
 import com.example.api.demo.spring.boot.mapper.UserMapper;
 import com.example.api.demo.spring.boot.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private UserMapper userMapper;
 
-    public User loginUser(UserLoginRequest request) {
-        if (!this.userRepository.existsByUsername(request.getUsername())) {
-            throw new AppException(ErrorCode.USER_NOT_EXISTED);
-        }
-
+    public boolean authenticate(UserLoginRequest request) {
         User user = this.userRepository.findByUsername(request.getUsername());
-
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(15);
-        String pwEncode = passwordEncoder.encode(request.getPassword());
 
-        if( user.getPassword().equals(pwEncode) ){
-            return user;
-        }
-
-        return null;
+        return passwordEncoder.matches(request.getPassword(), user.getPassword());
     }
 
 }
